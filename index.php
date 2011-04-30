103c209b1da12e9f8b3ee813c8fbd98fb21215b3
Alexander <nikolas.l.alexander@gmail.com>
2011-04-30 22:52:36 +0700

    Added tweaking for date timezone settings

--- index.php
+++ index.php
@@ -188,6 +188,21 @@
 	}
 
 /*
+|---------------------------------------------------------------
+| DEFAULT TIMEZONE
+|---------------------------------------------------------------
+|
+| Set the default timezone for date/time functions to use if
+| none is set on the server.
+|
+*/
+
+if( ! ini_get('date.timezone') )
+{
+   date_default_timezone_set('GMT');
+}
+
+/*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
  * --------------------------------------------------------------------
